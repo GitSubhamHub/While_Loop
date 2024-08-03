8e56292476b6f9baf8b1e070dad57f0de6ba7e42
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2024-08-03 17:06:05 +0530

    Add files via upload

---
+++ While Loop/Num.js
@@ -0,0 +1,8 @@
+// 1. Write a JavaScript program to print numbers from 1 to 50. using while loop
+
+var num=1
+
+while (num<=50) {
+    console.log(num);
+    num++;
+}
---
+++ While Loop/Table_of_Five.js
@@ -0,0 +1,8 @@
+// 2. Write a JavaScript program to print the multiplication table of 5 up to 10.
+
+var num=1
+
+while (num<=10) {
+    console.log("5"+"*"+num+":",num*5);
+    num++;
+}
