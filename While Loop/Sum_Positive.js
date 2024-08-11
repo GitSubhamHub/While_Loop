a0a701161ab48d91662879f20ae58c626c95f0dd
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2024-08-11 22:38:33 +0530

    Add files via upload

---
+++ While Loop/Sum_Positive.js
@@ -0,0 +1,8 @@
+// 4. Write a JavaScript program to find the sum of digits of a positive integer from 1 to 20.
+
+var num=1, sum=0
+while (num<=20) {               //change the value and check the result
+    sum = sum + num
+    num++;
+}
+console.log("Sum of Positive integer from 1 to 20 is: "+sum);
