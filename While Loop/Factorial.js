1e8fe5220b60a43aec5c000db48fa385fb914ff0
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2024-08-11 22:05:47 +0530

    Add files via upload

---
+++ While Loop/Factorial.js
@@ -0,0 +1,9 @@
+// 3. Write a JavaScript program to calculate the factorial of a given number.
+
+var sn=1, product=1
+
+while (sn <= 5) {
+    product = product*sn
+    sn++
+}
+console.log("Factorial of 5 is: "+product);
